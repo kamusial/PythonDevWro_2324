0e49cf8526ef7148ab2ef75ca627a6175e75fd9b
student-wroclaw <student-wroclaw@EDU-WSB.local>
2023-10-14 10:42:06 +0200

    1

---
+++ Zjazd1_podstawy/1_Sobota_razem/1_Kamil_print_zmienne.py
@@ -0,0 +1,27 @@
+print('Czesc "Michal"')   #komenda print
+print("She's so high")
+
+a = 5    #typ: int
+b = 7.5  #typ: float
+
+print('a')    # napisz literke a
+print(a)      # wypisz zmienną a
+
+c = a + b
+print(c)
+print(a + b)
+print(3 * (a - b))
+
+napis1 = 'piesek'    #typ: string
+napis2 = "kotek"     #typ: string
+
+print(napis1)    #wyświetl zmienną 'napis1'
+
+print(napis1 + napis2)    #dodaj napisy ->złącz napisy
+#print(napis1 * napis2)    #mnożenie napisów - niemożliwe
+print(napis1 * a)     #mnożenie napisu przez licznę całkowitą
+#print(napis1 * b)     #mnożene napisu przez liczbę zmiennoprzecinkową - niemożliwe
+
+print(type(a))      #napisz typ zmiennej 'a'
+print(type(b))      #napisz tym zmiennej 'b'
+print(type(napis1)) #napisz typ zmiennej 'napis1'
