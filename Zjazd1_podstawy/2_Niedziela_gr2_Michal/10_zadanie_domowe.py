5b375ff4bf3441e56f6c61ec7fea7c3619200ab8
myjniak <myjniak@gmail.com>
2023-10-15 15:29:42 +0200

    zad dom

---
+++ Zjazd1_podstawy/2_Niedziela_gr2_Michal/10_zadanie_domowe.py
@@ -0,0 +1,14 @@
+"""Zadania są mniej więcej w kolejności od najłatwiejszego do najtrudniejszego"""
+# zad 1: wyprintuj sumę największej i najmniejszej liczby w liście:
+a = [2, 52, 5, 7, 12, 67, 22, 31]
+
+# zad 2: wyprintuj ile razy w tekście poniżej występuje litera 'e'
+# zad 3: wyprintuj która litera występuje najczęściej
+b = "pip! dwudziestu uzurpatorow dusi padalcem dwa pudle"
+
+# zad 4: zamień pary elementów w liście 'a' miejscami, czyli powinno wyjść: [52, 2, 7, 5, 67, 12, 31, 22]
+
+# zad 5: zasymuluj dużego lotka - wylosuj 6 NIEPOWTARZAJĄCYCH SIĘ liczb od 1 do 49
+import random
+
+# zad 6: wyprintuj 4 najczęściej występujące litery w zmiennej 'b' jako 4-literowe słowo
