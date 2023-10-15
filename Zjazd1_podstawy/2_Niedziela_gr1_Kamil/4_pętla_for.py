63470a35307a19d4f5a7f9aaf628f470b0fdeeab
student-wroclaw <student-wroclaw@EDU-WSB.local>
2023-10-15 12:59:58 +0200

    1

--- Zjazd1_podstawy/2_Niedziela_gr1_Kamil/4_pętla_for.py
+++ Zjazd1_podstawy/2_Niedziela_gr1_Kamil/4_pętla_for.py
@@ -1,9 +1,30 @@
 my_list = ['kotek', 'piesek', 'szynka', 'sąsiad', 'biurko', 'I jeszcze coś']
 
+print('Pętla1')
 for i in range(5):    #pętla 5 razy, i = 0-4
     print(f'interacja nr {i+1}, i ma wartość {i}')
     print(my_list[i]+my_list[i])
 
+print('\npętla2')
 for i in range(3, 17, 2):      #od, do, krok
     print(f'interacja nr {i + 1}, i ma wartość {i}')
 
+print('\nPętla3')
+print('dlugość listy:', len(my_list))
+for i in range(len(my_list)):
+    print(my_list[i])
+    if len(my_list[i]) > 5:
+        print('długie słowo\n...................................')
+
+print('\nPętla4')
+for i in my_list:
+    print(i)
+    if len(i) > 5:
+        print('długie słowo\n...................................')
+
+print('\nPętla5 - czy dane slowo jest w liście?')
+word = 'mazurek'
+for i in my_list:
+    print(i)
+    if len(i) > 5:
+        print('długie słowo\n...................................')
