f45319284364efb23d3b142b3376b7ecf46f5a94
student-wroclaw <student-wroclaw@EDU-WSB.local>
2023-10-15 09:25:00 +0200

    1

---
+++ Zjazd1_podstawy/2_Niedziela_gr1_Kamil/tmp.py
@@ -0,0 +1,10 @@
+salary = 6000
+
+if salary > 10000:
+    print('wchodzisz w 2gi prog podatkowy')
+
+elif salary > 5000:
+    print('jestes w pierwszym progu podatkowym')
+
+elif salary < 5000:
+    print('bez podatku')
