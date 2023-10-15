0b0eb88afe8f434966a4c188f9b28a5a80341be0
student-wroclaw <student-wroclaw@EDU-WSB.local>
2023-10-15 10:00:37 +0200

    1

---
+++ Zjazd1_podstawy/2_Niedziela_gr1_Kamil/1_print.py
@@ -0,0 +1,13 @@
+print('Wprowadz imie')
+first_name = input()
+
+age = int(input('Wprowadz wiek:   '))
+
+print('Czesc', first_name, '.\nMasz', age, 'lat. Pelnoletni za', 18-age)  #złożony print, z nową linią
+
+print(f'Czesc {first_name}. Masz {age} lat. Pelnoletni za {18-age}.')    #f - formatowanie
+
+#program, który wita uzytkownika.
+#czesc albo dzien dobry, zaleznie od wieku
+#Sznaowny Panie albo Szanowna Pani - w zależnosci od płci
+
