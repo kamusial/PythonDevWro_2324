687fd3844230a1b29d3829dc503ee69b6c7b38fb
student-wroclaw <student-wroclaw@EDU-WSB.local>
2023-11-18 13:26:05 +0100

    1

--- Zjazd2_podstawy/2_Sobota_gr2_Kamil/7_program.py
+++ Zjazd2_podstawy/2_Sobota_gr2_Kamil/7_program.py
@@ -7,4 +7,44 @@
 # w przypadku niedostepnej nazwy uzytkownika, program proponuje nazwę
 # program sprawdza poziom skomplikowania hasła
 
+user_dict = {
+    'majki': '123',
+    'Kamil': '124',
+    'Kamil1': '234',
+    'Kamil11': '765',
+    'Kamil111': 'mama',
+    'Kamil001': 'eee',
+    'Rafcio': '876',
+    'Betty': 'betty'
+}
+def is_user_available(user):
+    if user not in user_dict.keys():
+        print(f'Nazwa {user} dozwolna')
+        return True
+    else:
+        print(f'Nazwa {user} NIEdozwolona')
+        return False
+
+def add_user(user):
+    while True:
+        passwd1 = input('Podaj haslo: ')
+        passwd2 = input('Potwierdź haslo: ')
+        if passwd1 == passwd2:
+            user_dict[user] = passwd1
+            break
+        else:
+            print('hasla nie pasuja, jeszcze raz')
+
+
+def suggest_username(user):
+    return user + '1'
+
+
+username = input('Podaj uzytkownika: ')
+if is_user_available(username):
+    add_user(username)
+else:
+    print(f'Sugeruje nazwę {suggest_username(username)}')
+    decision = input('Chcesz wprowadzić inną nazwę (N), czy skorzystać z zaproponowanej nazwy (P)? ')
+
 
