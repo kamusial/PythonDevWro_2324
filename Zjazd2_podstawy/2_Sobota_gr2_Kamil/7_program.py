bb208caa8bf25b4d37ac479a6b783c47c80d0da5
student-wroclaw <student-wroclaw@EDU-WSB.local>
2023-11-18 12:37:22 +0100

    1

---
+++ Zjazd2_podstawy/2_Sobota_gr2_Kamil/7_program.py
@@ -0,0 +1,10 @@
+# dana jest baza użytkowników z hasłami
+# program pozwala na dodanie uzytkownika oraz zmianę hasła
+# program sprawdza, czy nazwa uzytkownika dostępna
+# program wymaga dwukrotnego wprowadzenia hasła przy dodawaniu nowego uzytkownika
+# program pyta, czy zakończyć, czy kontynuować dodawanie uzytkowników
+
+# w przypadku niedostepnej nazwy uzytkownika, program proponuje nazwę
+# program sprawdza poziom skomplikowania hasła
+
+
