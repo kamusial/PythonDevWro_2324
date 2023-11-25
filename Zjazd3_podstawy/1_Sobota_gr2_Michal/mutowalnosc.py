c9c5da26260e37bf578c2aa1571d84ea75bf2753
myjniak <myjniak@gmail.com>
2023-11-25 12:20:40 +0100

    notki mutowalnosci

--- Zjazd3_podstawy/1_Sobota_gr2_Michal/mutowalnosc.py
+++ Zjazd3_podstawy/1_Sobota_gr2_Michal/mutowalnosc.py
@@ -4,10 +4,21 @@ Tupla - wór elementów - nie można jej zmieniać po utworzeniu
 Dict - wór par elementów - można je zmieniać w trakcie istnienia listy
 """
 
+# zmienne mutowalne są modyfikowalne wewnątrz funkcji:
 def modifier(x):
     x[1] = 3
 
 
 a = [1, 2]
 modifier(a)
-print(a)
+print(a)  # [1, 3]
+
+
+# zmienne niemutowalne już nie
+def no_modifier(x):
+    x = x + 3
+
+
+a = 5
+no_modifier(a)
+print(a)  # nadal 5!
