0472bc215bd3e39150483fba69f014eec3b71ce3
student-wroclaw <student-wroclaw@EDU-WSB.local>
2023-11-25 12:51:15 +0100

    1

--- Zjazd3_podstawy/1_Sobota_gr1_Kamil/5_program.py
+++ Zjazd3_podstawy/1_Sobota_gr1_Kamil/5_program.py
@@ -3,11 +3,25 @@
 # program sprawdza, czy nazwa uzytkownika jes wolna
 # jeśli nazwa zajęta, program proponuje własną nazwę
 
+def add_user(username):
+    while True:
+        passwd1 = input('Podaj haslo: ')
+        passwd2 = input('Powtorz haslo haslo: ')
+        if passwd1 == passwd2:
+            users_database[username] = passwd1
+            break
+        else:
+            print('Hasła nie pasują, jeszcze raz')
+
+
+
 users_database = {
     'Kamil': '123',
     'Oliwia': 'Oliwia123',
     'Patixxd': 'Pati1'
 }
+
+
 
 
 new_user = input('Podaj nazwe uzytkownika: ')
@@ -24,4 +38,5 @@ if new_user not in users_database:
 
 else:
     print('Nazwa zajeta')
+
 print(users_database)
