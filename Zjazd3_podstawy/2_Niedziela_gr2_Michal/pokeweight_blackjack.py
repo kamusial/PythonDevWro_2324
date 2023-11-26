56eb7541c88316a5ec2dda41cd5a78f12caa7894
myjniak <myjniak@gmail.com>
2023-11-26 15:28:15 +0100

    pokemonowe oczko wagowe klasami

--- Zjazd3_podstawy/2_Niedziela_gr2_Michal/pokeweight_blackjack.py
+++ Zjazd3_podstawy/2_Niedziela_gr2_Michal/pokeweight_blackjack.py
@@ -17,7 +17,6 @@ def get_random_pokemon() -> Pokemon:
     pokemon_data = response.json()
     return Pokemon(pokemon_data["name"], pokemon_data["weight"])
 
-
 """
 Jesteśmy w pokoju z pokemonami, i mamy wielką walizkę, w której
 możemy unieść 2000 wagi.
@@ -26,5 +25,44 @@ Jeśli powiemy "tak", to poinformuje nas ile miejsca w walizce nam zostało.
 Jeśli powiemy "tak" i skończy nam się miejsce w walizce - PRZEGRYWAMY.
 Jeśli powiemy "nie", gra się kończy i printuje pokemony, które
 zabieramy ze sobą do domu ^_^
+"""
 
+
+class PokeBlackJack:
+    def __init__(self):
+        self.capacity = 2000
+        self.suitcase = []
+
+    def get_pokemon(self):
+        pokemon = get_random_pokemon()
+        self.capacity -= pokemon.weight
+        self.suitcase.append(pokemon.name)
+        print(f"You found {pokemon.name}, it weighs {pokemon.weight}")
+        self._print_capacity()
+
+    def _print_capacity(self):
+        if self.capacity >= 0:
+            print(f"Remaining capacity: {self.capacity}")
+        else:
+            print("No capacity left")
+
+    @property
+    def is_lost(self):
+        return self.capacity <= 0
+
+
+game = PokeBlackJack()
+game.get_pokemon()
+while input("Do you want to take another Pokemon with you? (y/n) ") == "y":
+    game.get_pokemon()
+    if game.is_lost:
+        print("You lost :<")
+        break
+else:
+    print(f"Congrats, you took {game.suitcase} with you ;)")
+
+
+"""
+Sekcja else dla pętli 'while' i 'for' wydarzy się wtedy, gdy pętla zakończy się
+bez instrukcji 'break' (czyli 'naturalnie')
 """
