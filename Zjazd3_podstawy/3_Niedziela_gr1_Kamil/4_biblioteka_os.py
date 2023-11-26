0a32b94225cc8b6b711a06ca58babdccc614d7da
student-wroclaw <student-wroclaw@EDU-WSB.local>
2023-11-26 12:11:49 +0100

    1

--- Zjazd3_podstawy/3_Niedziela_gr1_Kamil/4_biblioteka_os.py
+++ Zjazd3_podstawy/3_Niedziela_gr1_Kamil/4_biblioteka_os.py
@@ -1,14 +1,17 @@
 import os
 import time
 
+# import socket
+# hostname = socket.gethostname()
+# IPAddr = socket.gethostbyname(hostname)
+# print("Your Computer Name is:" + hostname)
+# print("Your Computer IP Address is:" + IPAddr)
+
 print(os.getcwd())
 os.chdir('C:\\Users\\vdi-student\\Desktop\\')
 print(os.getcwd())
-time.sleep(2)
 os.mkdir('new_folder')
-time.sleep(2)
 os.rename('new_folder', 'NOWSZY_FOLDER')
-time.sleep(2)
-os.rmdir('NOWSZY_FOLDER')
+# os.rmdir('NOWSZY_FOLDER')
 #os.system('cmd /c "dir /s new.txt >> result.txt"')
-os.system('cmd /c "ipconfig /all > result.txt"')
+os.system('cmd /c "cd C:\\Users\\vdi-student\\Desktop\\NOWSZY_FOLDER\\  && ipconfig /all > result.txt"')
---
+++ Zjazd3_podstawy/3_Niedziela_gr1_Kamil/6_zbiory.py
@@ -0,0 +1,13 @@
+# załóżmy, że pesel ma 4 cyfry
+# stwórzmy zbiór NFZ – ludzie, w bazie pacjentów NFZ
+# stwórzmy zbiór chorzy_rok – ludzie chorzy w ostatnim roku
+# stwórzmy zbiór chorzy_miesiac – ludzie chorzy w ostatnim miesiącu
+# stwórzmy zbiór centrum – wszystkich ludzi mieszkających na bemowie
+# stwórzmy zbiór krzyki – wszystkich ludzi mieszkających na zoliborzu
+
+NFZ = {1234, 3476, 4544, 3423, 3254, 8769, 5436, 2345, 6532, 1243, 6435, 1298, 6732, 7688, 7648, 2345, 2356}
+chorzy_rok = set([1234, 3476, 4544, 3423, 3254, 8769, 5436])
+chorzy_miesiac = set([1234, 3476, 3098, 4544, 3423])
+centrum = {4544, 3423, 3254, 8769, 5436, 2345, 6532, 1243}
+krzyki = {7648, 2345, 2356, 3987, 1234, 3476, 3254}
+zbior_pusty = set()
