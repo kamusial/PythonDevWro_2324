b2446d043be73f5661f4300413dda57cfd310ef1
student-wroclaw <student-wroclaw@EDU-WSB.local>
2023-12-09 11:07:41 +0100

    1

--- Zjazd4_podstawy/1_Sobota_Gr1_Kamil/1_obsuga_txt.py
+++ Zjazd4_podstawy/1_Sobota_Gr1_Kamil/1_obsuga_txt.py
@@ -2,8 +2,7 @@
 
 from functions import *
 
-with open('u2.txt', 'r', encoding='utf8') as file1:
-    content = file1.read()
-
+content = read_file('u2.txt')
+content = clear_text(content)
 print(f'Liczba slow: {no_of_words(content)}')
 print(f'liczba roznych slow: {no_of_unique_words(content)}')
--- Zjazd4_podstawy/1_Sobota_Gr1_Kamil/functions.py
+++ Zjazd4_podstawy/1_Sobota_Gr1_Kamil/functions.py
@@ -9,7 +9,21 @@ def no_of_unique_words(text):
     return len(text)
 
 def clear_text(text):
-    character = ['.', ',', '(', ')', '"']
+    character = set('.,(\'\"')
     for char in character:
         text = text.replace(char, '')
     return text
+
+def read_file(filename):
+    with open(filename, 'r', encoding='utf8') as file1:
+        content = file1.read()
+    return content
+
+def words_repeat(text):
+    text = text.split()
+    my_dict = {}
+    for word in text:
+        if word in my_dict.keys():
+            my_dict[word] += 1
+        else:
+            my_dict[word] = 1
