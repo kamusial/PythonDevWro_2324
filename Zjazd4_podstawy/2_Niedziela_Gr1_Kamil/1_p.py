6321d17a04682fdf06d67a8ec88cc6ee8061af8e
student-wroclaw <student-wroclaw@EDU-WSB.local>
2023-12-10 09:49:14 +0100

    1

--- Zjazd4_podstawy/2_Niedziela_Gr1_Kamil/1_p.py
+++ Zjazd4_podstawy/2_Niedziela_Gr1_Kamil/1_p.py
@@ -1,14 +1,23 @@
 import argparse
 
-parser = argparse.ArgumentParser()
+parser = argparse.ArgumentParser(description='Program czyta plik tekstowy')
 
-parser.add_argument('filename')
+parser.add_argument('filename', help='nazwa pliku bez rozszerzenia')
+parser.add_argument('-m', '--mode', default='r',
+                    help='tryb, domyslnie "r" do odczytu')
+parser.add_argument('-ex', '--extention', default='txt',
+                    help='rozszerzenie, domyslnie ".txt"')
+parser.add_argument('-t', '--test')
 
 args = parser.parse_args()
 
 print(f'Nazwa pliku to: {args.filename}')
+print(f'Tryb to: {args.mode}')
+print(f'Rozszerzenie to: {args.extention}')
+print(f'Testowy argument to: {args.test}')
 
-with open(args.filename, 'r') as file1:
+file = args.filename + '.' + args.extention
+print(file)
+with open(file, args.mode) as file1:
     content = file1.read()
-
 print(content)
