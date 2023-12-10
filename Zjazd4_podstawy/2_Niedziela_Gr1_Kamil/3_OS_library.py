9d8b75992e54793d884b23c93a1419498f61325c
student-wroclaw <student-wroclaw@EDU-WSB.local>
2023-12-10 13:10:40 +0100

    1

--- Zjazd4_podstawy/2_Niedziela_Gr1_Kamil/3_OS_library.py
+++ Zjazd4_podstawy/2_Niedziela_Gr1_Kamil/3_OS_library.py
@@ -1,9 +1,19 @@
 import os
+import time
 
+ts1 = time.time()
 print(os.getcwd())
 os.chdir('C:\\Users\\vdi-student\\Desktop\\')
 print(os.getcwd())
+time.sleep(1)
 os.mkdir('Nowy_folder')
-
-
+time.sleep(1)
+os.rename('Nowy_folder', 'NOWSZY_folder')
+time.sleep(1)
+os.rmdir('NOWSZY_folder')
+ts2 = time.time()
+print(f'czas trwania {ts2-ts1}')
 #print(os.environ)
+
+os.system('cmd /c "ipconfig "')
+
