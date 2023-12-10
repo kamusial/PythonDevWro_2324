1049e02fbc1265fe3d2392a4562806f177adb006
student-wroclaw <student-wroclaw@EDU-WSB.local>
2023-12-10 14:22:54 +0100

    1

--- Zjazd4_podstawy/2_Niedziela_Gr1_Kamil/4_selenium_basic.py
+++ Zjazd4_podstawy/2_Niedziela_Gr1_Kamil/4_selenium_basic.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 from time import sleep
 
 driver = webdriver.Chrome()
@@ -8,5 +9,11 @@ sleep(1)
 button1_accept = driver.find_element('id', 'L2AGLb')
 button1_accept.click()
 sleep(1)
-
+search_field = driver.find_element('name','q')
+search_field.send_keys('Czy github dziala z MACem?')
+sleep(1)
+search_field.send_keys(Keys.ENTER)
+# search_button = driver.find_element('name', 'btnK')
+# search_button.submit()
+sleep(1)
 driver.quit()
