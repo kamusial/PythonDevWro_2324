370add6879ee1fc9d423164a97aeee7e973e4e04
student-wroclaw <student-wroclaw@EDU-WSB.local>
2023-12-10 14:52:36 +0100

    1

--- Zjazd4_podstawy/2_Niedziela_Gr1_Kamil/5_selenium_basic2.py
+++ Zjazd4_podstawy/2_Niedziela_Gr1_Kamil/5_selenium_basic2.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from time import sleep
+import datetime
 
 driver = webdriver.Chrome()
 driver.get('https://google.com')
@@ -9,11 +10,15 @@ sleep(1)
 button1_accept = driver.find_element('id', 'L2AGLb')
 button1_accept.click()
 sleep(1)
-search_field = driver.find_element('name','q')
+search_field = driver.find_element('name', 'q')
 search_field.send_keys('Czy github dziala z MACem?')
 sleep(1)
 search_field.send_keys(Keys.ENTER)
 # search_button = driver.find_element('name', 'btnK')
 # search_button.submit()
 sleep(1)
+
+teraz = datetime.datetime.now()
+filename = teraz.strftime('screenshot-%H_%M-%S.png')
+driver.get_screenshot_as_file(filename)
 driver.quit()
