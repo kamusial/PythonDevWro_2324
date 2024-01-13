62cadb1a3c944feadab72a254d36c8f9a93c56ed
student-wroclaw <student-wroclaw@EDU-WSB.local>
2024-01-13 09:02:54 +0100

    1

---
+++ Zjazd_6/1_Kamil_sobota/0_tmp.py
@@ -0,0 +1,23 @@
+# wykresy
+# matplotlib.pyplot
+
+# ładne wykresy
+# seabourn
+
+# czytanie danych, praca z danymi
+# pandas
+
+# sztuczna integencja
+# tensorflow, keras
+
+# testy web
+# selenium
+
+# matematyka, obliczenia
+# numpy, scipy
+
+# grafika
+# tkinter, pyqt6
+
+# gry
+# pygame
