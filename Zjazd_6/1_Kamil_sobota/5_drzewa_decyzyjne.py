9e441e05d794b65ce08114b7dd40f0241fa3c25b
student-wroclaw <student-wroclaw@EDU-WSB.local>
2024-01-14 11:17:09 +0100

    1

--- Zjazd_6/1_Kamil_sobota/5_drzewa_decyzyjne.py
+++ Zjazd_6/1_Kamil_sobota/5_drzewa_decyzyjne.py
@@ -3,8 +3,9 @@ import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
-from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import confusion_matrix
+from mlxtend.plotting import plot_decision_regions
 
 df = pd.read_csv('iris.csv')
 print(df['class'].value_counts())
@@ -25,28 +26,20 @@ sample = np.array([5.6, 3.2, 5.2, 1.45])    # mój liść
 # sns.scatterplot(data=df, x='petallength', y='petalwidth', hue='class')
 # plt.show()
 
-df['distance'] = (df.sepallength-sample[0]) ** 2 + (df.sepalwidth - sample[1]) ** 2 + \
-                 (df.petallength - sample[2]) ** 2 + (df.petalwidth - sample[3]) ** 2
-
-print(df.sort_values('distance').head(10))
-
-print(df.head().to_string())
-X = df.iloc[  :  , 0  : 4  ]
+X1 = df[['sepallength', 'sepalwidth']]
+X2 = df[['petallength', 'petalwidth']]
+X3 = df.iloc[:,:4]
 y = df.class_value
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+# X_train, X_test, y_train, y_test = train_test_split(X3, y, test_size=0.2)
 
-# model = KNeighborsClassifier(n_neighbors=50, weights='distance')
-# model.fit(X_train, y_train)
+model = DecisionTreeClassifier(max_depth=5, min_samples_split=2)
+model.fit(X3, y)
+
 # print(model.score(X_test, y_test))
 # print(pd.DataFrame(confusion_matrix(y_test, model.predict(X_test))))
 
+# plot_decision_regions(X2.values, y.values, model)
+# plt.show()
 
-results = []
-for k in range(1, 51):
-    model = KNeighborsClassifier(n_neighbors=k, weights='distance')
-    model.fit(X_train, y_train)
-    results.append(model.score(X_test, y_test))
-plt.plot(range(1, 51), results)
-plt.grid()
-plt.show()
+print(pd.DataFrame(model.feature_importances_, X3.columns))
