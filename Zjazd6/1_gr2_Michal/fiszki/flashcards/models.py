9a0c9f6258d886f55c1b2e62b3ff40647150cda6
myjniak <myjniak@gmail.com>
2024-01-27 13:56:51 +0100

    13:56

--- Zjazd6/1_gr2_Michal/fiszki/flashcards/models.py
+++ Zjazd6/1_gr2_Michal/fiszki/flashcards/models.py
@@ -5,3 +5,7 @@ class Flashcard(models.Model):
     name = models.CharField(max_length=200)
     question = models.CharField(max_length=500)
     answer = models.CharField(max_length=1000)
+    slug = models.SlugField(max_length=200, blank=True, null=True)
+
+    def __str__(self):
+        return self.name
