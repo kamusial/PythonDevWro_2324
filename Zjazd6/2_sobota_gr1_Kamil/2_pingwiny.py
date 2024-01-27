1f9b8c217e6771f0390bbbb98ee4b69993829a79
student-wroclaw <student-wroclaw@EDU-WSB.local>
2024-01-27 13:47:38 +0100

    1

--- Zjazd6/2_sobota_gr1_Kamil/2_pingwiny.py
+++ Zjazd6/2_sobota_gr1_Kamil/2_pingwiny.py
@@ -25,9 +25,22 @@ X_train, X_test, y_train, y_test = train_test_split(penguins_features, target, t
 inputs = keras.Input(X_train.shape[1])
 hidden_layer1 = keras.layers.Dense(4, activation='relu')(inputs)
 hidden_layer2 = keras.layers.Dense(4, activation='relu')(hidden_layer1)
-output_layer = keras.layers.Dense(4, activation='relu')(hidden_layer2)
+output_layer = keras.layers.Dense(3, activation='softmax')(hidden_layer2)
 
 model = keras.Model(inputs=inputs, outputs=output_layer)
 # print(model.summary())
 
 model.compile(optimizer="rmsprop", loss=keras.losses.categorical_crossentropy)
+result = model.fit(X_train, y_train, epochs=100)
+sns.lineplot(x=result.epoch, y=result.history['loss'])
+plt.show()
+
+y_pred = model.predict(X_test)
+prediction = pd.DataFrame(y_pred, columns=target.columns)
+predicted_species = prediction.idxmax(axis='columns')
+print(predicted_species)
+
+from sklearn.metrics import confusion_matrix
+true_species = y_test.idxmax(axis='columns')
+matrix = confusion_matrix(true_species, predicted_species)
+print(matrix)
