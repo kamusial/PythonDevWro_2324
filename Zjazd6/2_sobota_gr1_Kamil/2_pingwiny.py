f86f74a68c9beca438f0a274ac2d80c5c6f53a4c
student-wroclaw <student-wroclaw@EDU-WSB.local>
2024-01-27 14:28:06 +0100

    1

--- Zjazd6/2_sobota_gr1_Kamil/2_pingwiny.py
+++ Zjazd6/2_sobota_gr1_Kamil/2_pingwiny.py
@@ -20,18 +20,19 @@ seed(1)
 from tensorflow.random import set_seed
 set_seed(2)
 
-X_train, X_test, y_train, y_test = train_test_split(penguins_features, target, test_size=0.2)
+X_train, X_test, y_train, y_test = train_test_split(penguins_features, target, test_size=0.2, random_state=0)
 
 inputs = keras.Input(X_train.shape[1])
-hidden_layer1 = keras.layers.Dense(10, activation='relu')(inputs)
-hidden_layer2 = keras.layers.Dense(10, activation='relu')(hidden_layer1)
-output_layer = keras.layers.Dense(3, activation='softmax')(hidden_layer2)
+hidden_layer1 = keras.layers.Dense(10, activation='linear')(inputs)
+hidden_layer2 = keras.layers.Dense(10, activation='linear')(hidden_layer1)
+hidden_layer3 = keras.layers.Dense(10, activation='linear')(hidden_layer2)
+output_layer = keras.layers.Dense(3, activation='softmax')(hidden_layer3)
 
 model = keras.Model(inputs=inputs, outputs=output_layer)
 # print(model.summary())
 
-model.compile(optimizer="rmsprop", loss=keras.losses.categorical_crossentropy)
-result = model.fit(X_train, y_train, epochs=500, verbose=0)
+model.compile(optimizer="adam", loss=keras.losses.categorical_crossentropy)
+result = model.fit(X_train, y_train, epochs=1000, verbose=0)
 sns.lineplot(x=result.epoch, y=result.history['loss'])
 plt.show()
 
@@ -43,4 +44,8 @@ print(predicted_species)
 from sklearn.metrics import confusion_matrix
 true_species = y_test.idxmax(axis='columns')
 matrix = confusion_matrix(true_species, predicted_species)
-print(matrix)
+confusion_df = pd.DataFrame(matrix, index=y_test.columns.values, columns=y_test.columns.values)
+confusion_df.index.name = 'True label'
+confusion_df.columns.name = 'Predicted label'
+sns.heatmap(confusion_df, annot=True)
+plt.show()
