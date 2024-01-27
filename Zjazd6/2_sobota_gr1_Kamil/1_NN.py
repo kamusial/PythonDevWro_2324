bc57eb6d88639cf8b1801cbb0beff44203eeb3ce
student-wroclaw <student-wroclaw@EDU-WSB.local>
2024-01-27 11:44:05 +0100

    1

--- Zjazd6/2_sobota_gr1_Kamil/1_NN.py
+++ Zjazd6/2_sobota_gr1_Kamil/1_NN.py
@@ -9,17 +9,33 @@ from tensorflow.random import set_seed  # losowosc
 set_seed(0)  #te same wyniki
 
 model = Sequential()
-model.add(Dense(4, input_shape=[1], activation='linear'))
+model.add(Dense(1, input_shape=[1], activation='linear'))
 model.add(Dense(2, activation='linear'))
 model.add(Dense(1))
 
 # kompilacja
-model.compile(optizer='rmsprop', loss='mse', metrics=['mae'])
+model.compile(optimizer="rmsprop", loss="mse", metrics=["mae"])
 # mse - mean square error
 # mae - mean average error
 
+df = pd.read_csv('f-c.csv', usecols=[1, 2])
+print(df.head())
+
+import matplotlib.pyplot as plt
 
 
+result = model.fit(df.F, df.C, epochs=5000, verbose=0)
+print(result.history.keys())
+
+df1 = pd.DataFrame(result.history)
+print(df1.head())
+df1.plot()
+plt.show()
+
+y_pred = model.predict(df.F)   # policz stopnie C
+plt.scatter(df.F, df.C)
+plt.plot(df.F, y_pred, c='r')
+plt.show()
 
 
 
