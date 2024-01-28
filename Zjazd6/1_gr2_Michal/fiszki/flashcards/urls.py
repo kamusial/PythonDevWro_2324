ddcb8be773d0f10b23ec8c1ec866d5a572741f83
myjniak <myjniak@gmail.com>
2024-01-28 12:16:07 +0100

    delete fiszke

--- Zjazd6/1_gr2_Michal/fiszki/flashcards/urls.py
+++ Zjazd6/1_gr2_Michal/fiszki/flashcards/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path("", views.index),
     path("flashcards/items/<slug:slug>", views.learn_flashcard, name="flashcard-learn"),
-    path("flashcards", views.flashcard_list, name="flashcards-collection")
+    path("flashcards", views.FlashcardList.as_view(), name="flashcards-collection")
 ]
--- Zjazd6/1_gr2_Michal/fiszki/flashcards/views.py
+++ Zjazd6/1_gr2_Michal/fiszki/flashcards/views.py
@@ -2,6 +2,7 @@ from django.http import HttpRequest
 from django.shortcuts import render, get_object_or_404, redirect
 from django.urls import reverse
 from django.utils.text import slugify
+from django.views import View
 
 from .models import Flashcard
 from .forms import CreateFlashcardForm
@@ -16,6 +17,40 @@ def learn_flashcard(request, slug):
     return render(request, "learn-flashcard.html", context={"flashcard": flashcard})
 
 
+class FlashcardList(View):
+    def get(self, request: HttpRequest):
+        form = CreateFlashcardForm()
+        context = self._create_context(form)
+        return render(request, "flashcard-list.html", context=context)
+
+    def post(self, request: HttpRequest):
+        if request.POST.get("requestType") == "DELETE":
+            return self.delete(request)
+        form = CreateFlashcardForm(request.POST)
+        if form.is_valid():
+            new_flashcard = Flashcard(**form.cleaned_data,
+                                      slug=slugify(form.cleaned_data["name"]))
+            new_flashcard.save()
+            return redirect(reverse("flashcards-collection"))
+        context = self._create_context(form)
+        return render(request, "flashcard-list.html", context=context)
+
+    def delete(self, request: HttpRequest):
+        slug = request.POST.get("slug")
+        Flashcard.objects.filter(slug=slug).delete()
+        return redirect(reverse("flashcards-collection"))
+
+    def _create_context(self, form):
+        flashcards = Flashcard.objects.all()
+        context = {
+            "flashcards": flashcards,
+            "form": form
+        }
+        return context
+
+
+""" 
+jako funkcja:
 def flashcard_list(request: HttpRequest):
     form = CreateFlashcardForm()
     if request.method == "POST":
@@ -31,3 +66,4 @@ def flashcard_list(request: HttpRequest):
         "form": form
     }
     return render(request, "flashcard-list.html", context=context)
+"""
