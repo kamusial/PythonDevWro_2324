0fdb18f1573c129e05dc7ce9b4b2b52906e00a48
student-wroclaw <student-wroclaw@EDU-WSB.local>
2024-03-03 14:01:35 +0100

    1

--- Zjazf7_ML_Kamil_Gr2/SVM.py
+++ Zjazf7_ML_Kamil_Gr2/SVM.py
@@ -9,13 +9,28 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import confusion_matrix
 
 X, y = make_circles(n_samples=500, factor=0.5, noise=0.2)
-print(X)
-print(y)
-
 plt.scatter(X[:,0], X[:, 1], c=y)
 plt.show()
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.2)
+
+print('\Regresja logistyczna')
+model = LogisticRegression()
+model.fit(X_train, y_train)
+print(model.score(X_test, y_test))
+print(pd.DataFrame(confusion_matrix(y_test, model.predict(X_test))))
+
+print('\KNN')
+model = KNeighborsClassifier()
+model.fit(X_train, y_train)
+print(model.score(X_test, y_test))
+print(pd.DataFrame(confusion_matrix(y_test, model.predict(X_test))))
+
+print('\Drzewo decyzyjne')
+model = DecisionTreeClassifier()
+model.fit(X_train, y_train)
+print(model.score(X_test, y_test))
+print(pd.DataFrame(confusion_matrix(y_test, model.predict(X_test))))
 
 print('\SVM')
 model = SVC(kernel='rbf', degree=5)
