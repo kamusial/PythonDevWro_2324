4f07ccf9d9e8afcf03f8c998ed505cd8cdbd3e23
student-wroclaw <student-wroclaw@EDU-WSB.local>
2024-03-16 14:33:49 +0100

    11

--- Zjazd8_ML/UX_Gr2_Kamil/2_penguins.py
+++ Zjazd8_ML/UX_Gr2_Kamil/2_penguins.py
@@ -31,6 +31,26 @@ output_layer = keras.layers.Dense(3, activation="softmax")(hidden_layer3)
 model = keras.Model(inputs=inputs, outputs=output_layer)
 print(model.summary())
 
-# model.compile(optimizer='adam', loss=keras.losses.CategoricalCrossentropy())
-# history = model.fit(X_train, y_train, epochs=10)
+model.compile(optimizer='adam', loss=keras.losses.CategoricalCrossentropy())
+history = model.fit(X_train, y_train, epochs=10)
+sns.lineplot(x=history.epoch, y=history.history['loss'])
+plt.show()
+
+y_pred = model.predict(X_test)
+prediction = pd.DataFrame(y_pred, columns=target.columns)
+print(prediction)
+predicted_species = prediction.idxmax(axis='columns')
+
+from sklearn.metrics import confusion_matrix
+true_species = y_test.idxmax(axis='columns')
+
+matrix = confusion_matrix(true_species, predicted_species)
+confusion_df = pd.DataFrame(matrix, index=y_test.columns.values, columns=y_test.columns.values)
+confusion_df.index.name = 'True Label'
+confusion_df.columns.name = 'Predicted Label'
+sns.heatmap(confusion_df, annot=True)
+plt.show()
+
+
+
 
