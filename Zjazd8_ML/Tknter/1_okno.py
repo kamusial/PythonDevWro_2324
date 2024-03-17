da7d5c9051b63888e1a0fffd442cb54e1888bfcf
student-wroclaw <student-wroclaw@EDU-WSB.local>
2024-03-17 11:20:37 +0100

    1

--- Zjazd8_ML/Tknter/1_okno.py
+++ Zjazd8_ML/Tknter/1_okno.py
@@ -3,6 +3,14 @@ import tkinter as tk
 root = tk.Tk()
 root.title('Moja aplikacja')
 root.geometry('600x400+100+50')
+#root.resizable(True, False)  #można zmienić szerokosć okna, nie mozna zmienić wysokości
+root.minsize(200, 400)
+root.maxsize(800, 600)
+root.attributes('-alpha', 0.9)   # przeźroczystość
+root.attributes('-topmost', 1)   # zawsze na wierzchu
+root.i
+
+
 message = tk.Label(root, text='No siema')
 message.pack()
 
