000411cd49e22131a0c399d3541e759bddc90de5
student-wroclaw <student-wroclaw@EDU-WSB.local>
2024-03-17 09:01:56 +0100

    1

--- Zjazd8_ML/UX_Gr2_Kamil/3_ANN.py
+++ Zjazd8_ML/UX_Gr2_Kamil/3_ANN.py
@@ -1,5 +1,22 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from numpy.random import seed
 import pandas as pd
 from sklearn.datasets import load_breast_cancer  #, load_wine
 from sklearn.neural_network import MLPClassifier
+from collections import Counter
+
+################ Pandas - settings
+pd.set_option('display.max_rows', 5000)
+pd.set_option('display.max_columns', 5000)
+pd.set_option('display.width', 10000)
+pd.set_option('display.max_colwidth', 10000)
+np.set_printoptions(linewidth=2000)
+
+################ Cancer - analiza danych
+X, y = load_breast_cancer(return_X_y=True, as_frame=True)
+print(X.shape)
+print(X.columns)
+print(X.describe())
+
+print(Counter(y))  # zlicz
