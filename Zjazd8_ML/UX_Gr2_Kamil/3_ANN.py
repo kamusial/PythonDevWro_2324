aae6fa65a1fabfa1eb30c4017b6f4305a1c6d50c
student-wroclaw <student-wroclaw@EDU-WSB.local>
2024-03-17 09:42:42 +0100

    1

--- Zjazd8_ML/UX_Gr2_Kamil/3_ANN.py
+++ Zjazd8_ML/UX_Gr2_Kamil/3_ANN.py
@@ -5,6 +5,8 @@ import pandas as pd
 from sklearn.datasets import load_breast_cancer  #, load_wine
 from sklearn.neural_network import MLPClassifier
 from collections import Counter
+from sklearn.model_selection import train_test_split, RepeatedStratifiedKFold
+from sklearn.metrics import accuracy_score
 
 ################ Pandas - settings
 pd.set_option('display.max_rows', 5000)
@@ -18,5 +20,16 @@ X, y = load_breast_cancer(return_X_y=True, as_frame=True)
 print(X.shape)
 print(X.columns)
 print(X.describe())
-
 print(Counter(y))  # zlicz
+
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+model = MLPClassifier(hidden_layer_sizes=(100, 100), max_iter=100, activation='relu')
+model.fit(X_train, y_train)
+y_pred = model.predict(X_test)
+
+from sklearn.metrics import confusion_matrix
+
+print(confusion_matrix(y_test, y_pred))
+print(accuracy_score(y_test, y_pred))
+print(model.n_layers_)
