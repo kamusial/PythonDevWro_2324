bc8c44733f105268038c2448bc3e3a75f31a5cec
student-wroclaw <student-wroclaw@EDU-WSB.local>
2024-03-17 08:41:26 +0100

    1

---
+++ Zjazd8_ML/UX_Gr2_Kamil/3_ANN.py
@@ -0,0 +1,5 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from sklearn.datasets import load_breast_cancer  #, load_wine
+from sklearn.neural_network import MLPClassifier
