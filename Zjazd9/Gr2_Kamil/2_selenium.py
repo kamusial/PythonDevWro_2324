8e1a1f49c372c379f9b3a0d195caefb0ddea0072
student-wroclaw <student-wroclaw@EDU-WSB.local>
2024-04-13 11:06:45 +0200

    1

--- Zjazd9/Gr2_Kamil/2_selenium.py
+++ Zjazd9/Gr2_Kamil/2_selenium.py
@@ -5,6 +5,12 @@ import time
 import datetime
 from selenium.common.exceptions import NoSuchElementException
 
+def make_screenshot(driver):
+    today = datetime.datetime.today()
+    short_date = today.strftime('_stamp%H%M%S')
+    screen_name = 'screen' + short_date + '.png'
+    driver.get_screenshot_as_file(screen_name)
+
 driver = webdriver.Chrome()
 driver.maximize_window()
 driver.get('https://www.saucedemo.com/')
