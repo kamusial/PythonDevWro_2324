36e24c11c0a1b596c8fc78a5324eca4848463d5d
Michał Szajkowski <myjniak@gmail.com>
2024-04-14 10:24:49 +0200

    csv to avro

--- Zjazd9/Gr1_Szajkos/csv2avro.py
+++ Zjazd9/Gr1_Szajkos/csv2avro.py
@@ -1,14 +1,29 @@
 import csv
+from dataclasses import make_dataclass
+
 import fastavro
+from pydantic_avro.base import AvroBase
 
 
-def csv_to_avro(data_filepath: str,
-                schema_filepath: str
-                ):
-    with open(data_filepath) as file:
-        data = list(csv.DictReader(file))
-    print(data)
+def read_csv(filepath: str) -> list[dict]:
+    with open(filepath) as file:
+        return list(csv.DictReader(file))
+
+
+def create_schema_based_on_data(data: list[dict]):
+    columns = list(data[0].keys())
+    cols_with_types = [(col, str) for col in columns]
+    schema_class = make_dataclass("SchemaClass", cols_with_types, bases=(AvroBase,))
+    avro_schema = schema_class.avro_schema()
+    return avro_schema
+
+
+def csv_to_avro(data_filepath: str, output_filepath: str):
+    data = read_csv(data_filepath)
+    schema = create_schema_based_on_data(data)
+    with open(output_filepath, "wb") as file:
+        fastavro.writer(file, schema, data)
 
 
 if __name__ == '__main__':
-    csv_to_avro("data/data.csv", "")
+    csv_to_avro("data/data.csv", "data/output.avro")
