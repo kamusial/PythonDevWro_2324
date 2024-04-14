787a9a4d630857d1b92777f6953c7afc21e94873
Michał Szajkowski <myjniak@gmail.com>
2024-04-14 12:04:32 +0200

    shortest loop found! :)

--- Zjazd9/Gr1_Szajkos/logistics.py
+++ Zjazd9/Gr1_Szajkos/logistics.py
@@ -11,27 +11,36 @@ Musimy obliczyć, które 4 miasta rybak może najszybciej objechać,
 """
 from math import sqrt
 from logistics_utils.loader import load_cities, City
+from itertools import permutations
 
 
-class Map:
-    def distance(self, city_a: City, city_b: City) -> float:
-        x_distance = abs(city_a.x - city_b.x)
-        y_distance = abs(city_a.y - city_b.y)
-        return sqrt(x_distance**2 + y_distance**2)
+def distance(city_a: City, city_b: City) -> float:
+    x_distance = abs(city_a.x - city_b.x)
+    y_distance = abs(city_a.y - city_b.y)
+    return sqrt(x_distance**2 + y_distance**2)
 
-    def city_loop_distance(self, cities: list[City]) -> float:
-        """city_a, city_b, city_c, city_d"""
-        total_distance = 0
-        for i in range(len(cities)-1):
-            total_distance += self.distance(cities[i], cities[i+1])
-        total_distance += self.distance(cities[0], cities[-1])
-        return total_distance
+
+def city_loop_distance(cities: list[City]) -> float:
+    """city_a, city_b, city_c, city_d"""
+    total_distance = 0
+    for i in range(len(cities)-1):
+        total_distance += distance(cities[i], cities[i+1])
+    total_distance += distance(cities[0], cities[-1])
+    return total_distance
+
+
+def find_shortest_loop_of_cities(cities: list[City], loop_size: int):
+    perm = permutations(cities, loop_size)
+    results = []
+    for permutation in perm:
+        loop_distance = city_loop_distance(list(permutation))
+        results.append((permutation, loop_distance))
+    return min(results, key=lambda result: result[1])
 
 
 def main():
     cities = load_cities("data/cities.txt")
-    print(Map().city_loop_distance(cities[:4]))
-
+    print(find_shortest_loop_of_cities(cities, 4))
 
 
 if __name__ == '__main__':
