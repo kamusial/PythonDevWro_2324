c7346dc9fd412e3adcf2064d48b44ab619b68505
student-wroclaw <student-wroclaw@EDU-WSB.local>
2024-05-11 13:13:49 +0200

    1

--- Zjazd10/Gr1/Tkinter/16_OOP_program.py
+++ Zjazd10/Gr1/Tkinter/16_OOP_program.py
@@ -16,16 +16,42 @@ class MyGui:
         self.label.pack()
 
         self.textbox = tk.Text(self.root, height=5, font=('Arial', 15))
+        self.textbox.bind('<KeyPress>', self.short)
         self.textbox.pack(padx=10, pady=10)
 
-        self.check = tk.Checkbutton(self.root, text='show', font=('Arial', 15))
+        self.check_state = tk.IntVar()
+        self.check = tk.Checkbutton(self.root, text='show', font=('Arial', 15), variable=self.check_state)
         self.check.pack(padx=10, pady=10)
 
         self.button = tk.Button(self.root, text='Show message', font=('Arial', 15))
         self.button.pack(padx=10, pady=10)
 
+        self.root.protocol('WM_DELETE_WINDOW', self.zamknij)
 
         self.root.mainloop()
 
+        self.clearbtn = tk.Button(self.root, text='czysc', font=('Arial', 16), command=self.czysc)
+
+    def short(self, event):
+        # print('\nevent', event)
+        # print('keysym',event.keysym)
+        # print('state', event.state)
+        # print('znak',event.char)
+        if event.keysym == "Return" and event.state == 12:  #gdy ctrl+Enter
+            self.show_message()
+
+    def show_message(self):
+        print('Wiadomosc')
+        if self.check_state.get() == 0:   #jeśli checkbox NIE JEST zaznaczony
+            print(self.textbox.get('1.0', tk.END))  #cały tekst
+        else:
+            messagebox.showinfo(title='Message', message=self.textbox.get('1.0', tk.END))
+
+    def zamknij(self):
+        if messagebox.askyesno(title='Wyjsc?', message='Czy chcesz wyjsc?'):
+            self.root.destroy()   #zamknij okno
+
+    def czysc(self):
+        self.textbox.delete('1.0', tk.END)
 
 MyGui()
