5a72c5269577919433f0c4cda0fd00ef47d93c59
student-wroclaw <student-wroclaw@EDU-WSB.local>
2024-05-18 13:35:24 +0200

    1

--- Zjazd8_ML/PyQT6/3_button_names.py
+++ Zjazd8_ML/PyQT6/3_button_names.py
@@ -11,13 +11,34 @@ window_titles = [
     'Agnieszka',
     'Tomasz',
     'Ada',
-    'BLAD!'
 ]
+
+window_titles *= 2
+window_titles.append('BLAD!')
+
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
+        self.setWindowTitle('Aplikacja')
+        self.setFixedSize(600, 400)
+        self.button = QPushButton('Kliknij')
+        self.button.clicked.connect(self.button_clicked)
+        self.windowTitleChanged.connect(self.wrong_title)
+        self.setCentralWidget(self.button)
+
+    def button_clicked(self):
+        print('klikniety')
+        new_title = choice(window_titles)
+        print(f'nowy tytul: {new_title}')
+        self.setWindowTitle(new_title)
+
+    def wrong_title(self, window_title):
+        print('Tytul zmieniono na ',window_title)
+        if window_title == 'BLAD!':
+            self.button.setDisabled(True)
+            self.button.setText('Nieaktywny')
 
 app = QApplication(sys.argv)
 window = MainWindow()
