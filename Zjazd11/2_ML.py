e12f26cb3fb3f23499abebf26d897e5c26333bea
student-wroclaw <student-wroclaw@EDU-WSB.local>
2024-06-15 10:12:29 +0200

    1

--- Zjazd11/2_ML.py
+++ Zjazd11/2_ML.py
@@ -17,3 +17,36 @@ print(df.target.value_counts())
 X = df.iloc[:, :-1]
 y = df.target
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+print('\Part1 pojedyncze algorytmy')
+print('\nLogistic Regression')
+model = LogisticRegression()
+model.fit(X_train, y_train)
+print(model.score(X_test, y_test))
+print(pd.DataFrame(confusion_matrix(y_test, model.predict(X_test))))
+
+print('\nKNN')
+model = KNeighborsClassifier()
+model.fit(X_train, y_train)
+print(model.score(X_test, y_test))
+print(pd.DataFrame(confusion_matrix(y_test, model.predict(X_test))))
+
+results1 = []
+for k in range(1, 50):
+    model = KNeighborsClassifier(n_neighbors=k) # , weights='distance')
+    model.fit(X_train, y_train)
+    results1.append(model.score(X_test, y_test))
+
+plt.title('KNN')
+plt.plot(range(1, 50), results1)
+plt.show()
+
+results2 = []
+for k in range(1, 50):
+    model = KNeighborsClassifier(n_neighbors=k, weights='distance')
+    model.fit(X_train, y_train)
+    results2.append(model.score(X_test, y_test))
+
+plt.title('KNN - distance')
+plt.plot(range(1, 50), results2)
+plt.show()
